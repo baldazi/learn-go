ebe270f16df10f88703539d1a69f4589c69cd727
baldazi <baldaazi99@gmail.com>
2025-06-09 16:17:38 +0200

    basics

--- microservice/basics/cmd/main.go
+++ microservice/basics/cmd/main.go
@@ -1,11 +1,33 @@
 package main
 
 import (
+	"fmt"
+	"strconv"
 	"net/http"
 	"os"
 	"runtime"
 	"github.com/gin-gonic/gin"
 )
+
+func endPointHandler (ctx *gin.Context){
+	ctx.String(http.StatusOK, "%s; %s", ctx.Request.Method, ctx.Request.URL.Path)
+}
+
+func add(ctx *gin.Context){
+	x, err := strconv.ParseFloat(ctx.Param("x"), 64)
+	if err != nil{
+		ctx.String(http.StatusBadRequest, "%s is not a number", ctx.Param("x"))
+		return
+	}
+
+	y, err := strconv.ParseFloat(ctx.Param("y"), 64);
+	if err != nil{
+		ctx.String(http.StatusBadRequest,"%s is not a number", ctx.Param("y"))
+		return
+	}
+
+	ctx.String(http.StatusOK, fmt.Sprintf("%f", x+y))
+}
 
 func main(){
 	r := gin.Default()
@@ -21,6 +43,13 @@ func main(){
 		ctx.JSON(http.StatusOK, gin.H{
 			"os": runtime.GOOS,
 		})
+	})
+	r.GET("/info/:id", endPointHandler)
+	r.GET("/add/:x/:y", add)
+	// group endpoint
+	ur := r.Group("/user")
+	ur.	GET("/name", func (ctx *gin.Context){
+		ctx.String(http.StatusOK, "baldazi")
 	})
 	r.Run(":" + os.Getenv("PORT"))
 }
